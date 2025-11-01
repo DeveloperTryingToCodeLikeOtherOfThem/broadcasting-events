fb081b45b8b68969b234a1fd86c2a092b8234012
a012 <Aeagan0507@gmail.com>
2025-11-01 18:28:55 -0400

    Update events.ts

--- events.ts
+++ events.ts
@@ -10,7 +10,7 @@ namespace events {
 
 
      //% blockId="events_on_receive_broadcast" block="on receive $message"
-    //% color.shadow="color_enum_shim"
+    //% message.shadow="color_enum_shim"
    //% weight=99
     export function onReceive(message: BroadcastMessage, handler: () => void) {
         if (_id === message) handler()
@@ -19,7 +19,7 @@ namespace events {
     
    //% blockId=events_broadcast
     //% block="broadcast $message"
-    //% color.shadow="color_enum_shim" weight=100
+    //% message.shadow="color_enum_shim" weight=100
     export function broadcast(message: BroadcastMessage) {
         _id = message
     }
