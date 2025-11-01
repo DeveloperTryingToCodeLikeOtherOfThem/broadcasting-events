aba4126e0af4032f265db5b9ce68ce3733401206
a012 <Aeagan0507@gmail.com>
2025-11-01 18:24:17 -0400

    Initial files for MakeCode project

---
+++ events.ts
@@ -0,0 +1,27 @@
+namespace events {
+   export enum BroadcastMessage {
+       //% block="message1"
+      Message1,
+      //% block="message2"
+      Message2
+   }
+
+    let _id: BroadcastMessage
+
+
+  //% shim=ENUM_GET
+     //% blockId="events_on_receive_broadcast" block="on receive $message"
+    //% color.shadow="color_enum_shim"
+   //% weight=99
+    export function onReceive(message: BroadcastMessage, handler: () => void) {
+        if (_id === message) handler()
+        else return
+    }
+    
+   //% blockId=events_broadcast
+    //% block="broadcast $message"
+    //% color.shadow="color_enum_shim" weight=100
+    export function broadcast(message: BroadcastMessage) {
+        _id = message
+    }
+}
